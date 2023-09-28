d7284de67b8f92ed64f95c32885bb674e8e32d72
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-09-27 21:38:05 -0300

    style: add style global provider in aplication

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,39 @@
+type Colors = {
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  quartenary: string;
+  background: string;
+  text: string;
+  disabledText: string;
+};
+
+type Fonts = {
+  title: string;
+  text: string;
+  subtitle: string;
+  logo: string;
+};
+
+export type ThemeType = {
+  colors: Colors;
+  fonts: Fonts;
+};
+
+export const Theme: ThemeType = {
+  colors: {
+    primary: "#00AD6F",
+    secondary: "#80FF00",
+    tertiary: "#FFFFFF",
+    quartenary: "#585F6F",
+    background: "#A2C614",
+    text: "#1B1B1B",
+    disabledText: "#d3d3d3",
+  },
+  fonts: {
+    title: "Croissant One, cursive",
+    text: "Sofia Sans Condensed, sans-serif",
+    subtitle: "Dosis, sans-serif",
+    logo : "Pixelify Sans, cursive"
+  },
+};
