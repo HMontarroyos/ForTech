b4250f05499e2e2e6e0693efa431f6ef2204908c
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-10-01 13:12:22 -0300

    refactor: remove component footer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,11 +8,9 @@ import ModalPieces from "./ModalPieces";
 
 const Header = lazy(async () => await import("./Header"));
 const Loading = lazy(async () => await import("./Loading"));
-const Footer = lazy(async () => await import("./Footer"));
 
 export {
   Header,
-  Footer,
   Loading,
   CardPieces,
   GridPieces,
